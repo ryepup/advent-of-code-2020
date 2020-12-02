e96a0f06ad9944397781dee61b36898fcbbcec02
Ryan Davis <ryepup@gmail.com>
2020-12-01 19:26:45 -0500

    Day1: solve the test case

--- src/day1.rs
+++ src/day1.rs
@@ -1,3 +1,25 @@
-pub fn hello() -> u32 {
-    1
+use std::collections::BTreeSet;
+use std::fs::File;
+use std::io;
+use std::io::prelude::*;
+use std::io::BufReader;
+use std::path::Path;
+
+pub fn solve(input: &'static str) -> Result<u32, io::Error> {
+    let path = Path::new(input);
+    let file = File::open(&path)?;
+    let reader = BufReader::new(file);
+    let mut entries = BTreeSet::new();
+    for line in reader.lines() {
+        if let Ok(entry) = line {
+            println!("read '{}'", entry);
+            let n = u32::from_str_radix(&entry, 10).expect("line was not a number");
+            let target = 2020 - n;
+            if entries.contains(&target) {
+                return Ok(n * target);
+            }
+            entries.insert(n);
+        }
+    }
+    panic!("no solution found");
 }
--- tests/day1.rs
+++ tests/day1.rs
@@ -1,6 +1,6 @@
 use advent2020::day1;
 
 #[test]
-fn hello_returns_1() {
-    assert_eq!(1, day1::hello());
+fn solve() {
+    assert_eq!(514579, day1::solve("./tests/day1.test").unwrap());
 }
