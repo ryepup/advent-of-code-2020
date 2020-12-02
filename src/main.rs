440075890d93f5e3aa0fda4795cc11bd67657734
Ryan Davis <ryepup@gmail.com>
2020-12-02 14:18:16 -0500

    solution for pt 2

--- src/main.rs
+++ src/main.rs
@@ -2,5 +2,5 @@ use advent2020::day2;
 
 fn main() {
     println!("Part 1: {}", day2::solve("./data/day2.txt"));
-    //    println!("Part 2: {}", day1::solve2("./data/day1.txt"));
+    println!("Part 2: {}", day2::solve2("./data/day2.txt"));
 }
