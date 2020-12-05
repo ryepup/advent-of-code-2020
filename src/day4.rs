c2bf3b766a593c410ebfb1e649789de5e9ec1741
Ryan Davis <ryepup@gmail.com>
2020-12-05 12:54:39 -0500

    day 4 pt 2

--- src/day4.rs
+++ src/day4.rs
@@ -1,4 +1,7 @@
+extern crate regex;
+
 use crate::utils::read_lines;
+use regex::Regex;
 use std::collections::HashMap;
 use std::str::FromStr;
 
@@ -48,7 +51,14 @@ impl Iterator for PassportReader {
         loop {
             let line = match self.lines.next() {
                 Some(s) => s,
-                None => return None,
+                None => {
+                    if pairs.len() == 0 {
+                        return None;
+                    } else {
+                        // must have been the last line
+                        return Some(pairs.into());
+                    }
+                }
             };
             if line.len() == 0 {
                 return Some(pairs.into());
@@ -78,6 +88,55 @@ pub fn solve(filepath: &'static str) -> usize {
     reader.filter(is_valid).count()
 }
 
-pub fn solve2(filepath: &'static str) -> u32 {
-    todo!()
+fn is_valid_height<'a>(hgt: &'a &String) -> bool {
+    // hgt (Height) - a number followed by either cm or in:
+    lazy_static! {
+        static ref RE: Regex = Regex::new(r"^(?P<hgt>\d+)(?P<uom>cm|in)$").unwrap();
+    }
+    if let Some(caps) = RE.captures(&hgt) {
+        let n = caps["hgt"].parse::<u8>().unwrap();
+        match &caps["uom"] {
+            // If in, the number must be at least 59 and at most 76.
+            "in" => (59..=76).contains(&n),
+            // If cm, the number must be at least 150 and at most 193.
+            "cm" => (150..=193).contains(&n),
+            _ => false,
+        }
+    } else {
+        false
+    }
+}
+
+fn is_really_valid(passport: &Passport) -> bool {
+    lazy_static! {
+        // hcl (Hair Color) - a # followed by exactly six characters 0-9 or a-f.
+        static ref HCL_RE: Regex = Regex::new(r"^#[0-9a-f]{6}$").unwrap();
+        // ecl (Eye Color) - exactly one of: amb blu brn gry grn hzl oth.
+        static ref ECL_RE: Regex = Regex::new(r"^(amb|blu|brn|gry|grn|hzl|oth)$").unwrap();
+        // a nine-digit number, including leading zeroes.
+        static ref PID_RE: Regex = Regex::new(r"^[0-9]{9}$").unwrap();
+    }
+
+    // byr (Birth Year) - four digits; at least 1920 and at most 2002.
+    let is_valid = passport.byr.filter(|x| (1920..=2002).contains(x)).is_some()
+    // iyr (Issue Year) - four digits; at least 2010 and at most 2020.
+    && passport.iyr.filter(|x| (2010..=2020).contains(x)).is_some()
+    // eyr (Expiration Year) - four digits; at least 2020 and at most 2030.
+    && passport.eyr.filter(|x| (2020..=2030).contains(x)).is_some()
+    && passport.hgt.as_ref().filter(is_valid_height).is_some()
+    && passport.hcl.as_ref().filter(|x| HCL_RE.is_match(x)).is_some()
+    && passport.ecl.as_ref().filter(|x| ECL_RE.is_match(x)).is_some()
+    && passport.pid.as_ref().filter(|x| PID_RE.is_match(x)).is_some();
+
+    println!("is_really_valid {} {:?}", is_valid, passport);
+
+    is_valid
+}
+
+pub fn solve2(filepath: &'static str) -> usize {
+    println!("solving {}", filepath);
+    let reader = PassportReader {
+        lines: Box::new(read_lines(filepath)),
+    };
+    reader.filter(is_really_valid).count()
 }
--- tests/day4.rs
+++ tests/day4.rs
@@ -8,6 +8,7 @@ fn solve() {
 
 #[test]
 fn solve2() {
-    assert_eq!(0, day4::solve2("./data/day4.test.txt"));
-    assert_eq!(0, day4::solve2("./data/day4.txt"));
+    assert_eq!(0, day4::solve2("./data/day4.test2.invalid.txt"));
+    assert_eq!(4, day4::solve2("./data/day4.test2.valid.txt"));
+    assert_eq!(121, day4::solve2("./data/day4.txt"));
 }
